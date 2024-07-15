1f74d4fd2ac8ca333548b221654dc51e559cee72
codingwithkaraca <alperkaraca@ALPERs-MacBook-Air.local>
2024-07-15 08:46:47 +0300

    temizlendi ve yeni klasörler eklendi

--- frontend/duzeltme.php
+++ frontend/duzeltme.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taylor F</title>
 
-    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="./assets/css/duzeltme.css">
     <link rel="stylesheet" href="./assets/bootstrap/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/bootstrap-icons/font/bootstrap-icons.min.css">
 
