41fb78b0643b18bb2566ae527481ff194f49bd02
SÜMEYRA AYDEMİR <sumeyraaydemir@SUMEYRA-MacBook-Air.local>
2024-07-18 11:08:10 +0300

    SDG'ler eklendi.

--- frontend/index.php
+++ frontend/index.php
@@ -10,8 +10,8 @@ $password = "";
 $connect = mysqli_connect($host, $username, $password, $database);
 
 // Bağlantı hatası kontrolü
-if (!$connect){
-    die("Bağlantı hatası :". mysqli_connect_error());
+if (!$connect) {
+    die("Bağlantı hatası :" . mysqli_connect_error());
 }
 
 // Arama sorgusunu alma
@@ -105,12 +105,27 @@ $result = $statement->get_result(); // Sonuçları al
                 <option value="publisher">Yayınevi</option>
                 <option value="summary">Özet</option>
             </select>
-            <select name="category" class="category-dropdown">
-                <option value="all">SDG's</option>
-                <option value="bookname"></option>
-                <option value="authorname">Yazar</option>
-                <option value="publisher">Yayınevi</option>
-                <option value="summary">Özet</option>
+            <select id="sdg-select" name="sdg-select">
+                <option value="">SDG'ler</option>
+                <option value="1">1. Yoksulluğa Son (No Poverty)</option>
+                <option value="2">2. Açlığa Son (Zero Hunger)</option>
+                <option value="3">3. Sağlıklı ve Kaliteli Yaşam (Good Health and Well-being)</option>
+                <option value="4">4. Nitelikli Eğitim (Quality Education)</option>
+                <option value="5">5. Toplumsal Cinsiyet Eşitliği (Gender Equality)</option>
+                <option value="6">6. Temiz Su ve Sıhhi Koşullar (Clean Water and Sanitation)</option>
+                <option value="7">7. Erişilebilir ve Temiz Enerji (Affordable and Clean Energy)</option>
+                <option value="8">8. İnsana Yakışır İş ve Ekonomik Büyüme (Decent Work and Economic Growth)</option>
+                <option value="9">9. Sanayi, Yenilikçilik ve Altyapı (Industry, Innovation, and Infrastructure)</option>
+                <option value="10">10. Eşitsizliklerin Azaltılması (Reduced Inequality)</option>
+                <option value="11">11. Sürdürülebilir Şehirler ve Topluluklar (Sustainable Cities and Communities)
+                </option>
+                <option value="12">12. Sorumlu Üretim ve Tüketim (Responsible Consumption and Production)</option>
+                <option value="13">13. İklim Eylemi (Climate Action)</option>
+                <option value="14">14. Sudaki Yaşam (Life Below Water)</option>
+                <option value="15">15. Karasal Yaşam (Life on Land)</option>
+                <option value="16">16. Barış, Adalet ve Güçlü Kurumlar (Peace, Justice, and Strong Institutions)
+                </option>
+                <option value="17">17. Hedefler İçin Ortaklıklar (Partnerships for the Goals)</option>
             </select>
             <button type="submit" class="search-button">Ara</button>
         </form>
@@ -126,10 +141,10 @@ $result = $statement->get_result(); // Sonuçları al
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) { // Her sonuç satırını al
                     echo '<div class="article-card">'; // Makale kartı oluştur
-                    echo '<img src="'.$row["cover_img"]. '"  alt="'.$row["chapter_title"].'" class="article-image" width="250" height="150">'; // Kapak resmini ekle
-                    echo '<h3>'.$row["chapter_title"].'</h3>'; // Bölüm başlığını ekle
-                    echo '<p>'.$row["author_name"]. '</p>'; // Yazar adını ekle
-                    echo '<a href="'.$row["url"]. '">PDFi Görüntüle</a>'; // PDF bağlantısını ekle
+                    echo '<img src="' . $row["cover_img"] . '"  alt="' . $row["chapter_title"] . '" class="article-image" width="250" height="150">'; // Kapak resmini ekle
+                    echo '<h3>' . $row["chapter_title"] . '</h3>'; // Bölüm başlığını ekle
+                    echo '<p>' . $row["author_name"] . '</p>'; // Yazar adını ekle
+                    echo '<a href="' . $row["url"] . '">PDFi Görüntüle</a>'; // PDF bağlantısını ekle
                     echo '</div>';
                 }
             } else {
